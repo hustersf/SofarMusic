d2f134e4ab4cac16ea37a209dbeca4e2d27b1967
sufan <sofarsogood001@163.com>
2019-07-05 16:56:27 +0800

    引入greendao数据库

---
+++ external/daogenerator/src/main/java/com/sf/daogenerator/SongRecordDaoGenerator.java
@@ -0,0 +1,22 @@
+package com.sf.daogenerator;
+
+import org.greenrobot.greendao.generator.DaoGenerator;
+import org.greenrobot.greendao.generator.Entity;
+import org.greenrobot.greendao.generator.Schema;
+
+/**
+ * 存储音乐相关
+ */
+public class SongRecordDaoGenerator {
+
+  public static void main(String args[]) throws Exception {
+    Schema schema = new Schema(1, "com.sf.sofarmusic.db.song");
+
+    Entity entity = schema.addEntity("SongRecord");
+    entity.addLongProperty("id").primaryKey().autoincrement();
+    entity.addStringProperty("songId");
+
+    new DaoGenerator().generateAll(schema, "./packages/app/src/main/java");
+  }
+
+}
