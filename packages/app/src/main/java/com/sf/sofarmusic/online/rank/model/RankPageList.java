c2f0b1815aaf0f0654d2721795f5be678b6406d8
sufan <sofarsogood001@163.com>
2019-07-18 18:42:31 +0800

    榜单列表过滤掉 有问题的数据

--- packages/app/src/main/java/com/sf/sofarmusic/online/rank/model/RankPageList.java
+++ packages/app/src/main/java/com/sf/sofarmusic/online/rank/model/RankPageList.java
@@ -2,7 +2,7 @@ package com.sf.sofarmusic.online.rank.model;
 
 import com.sf.base.network.retrofit.page.SofarRetrofitPageList;
 import com.sf.sofarmusic.api.ApiProvider;
-
+import java.util.List;
 import io.reactivex.Observable;
 
 public class RankPageList extends SofarRetrofitPageList<RankResponse, Rank> {
@@ -15,5 +15,16 @@ public class RankPageList extends SofarRetrofitPageList<RankResponse, Rank> {
   @Override
   protected boolean allowDuplicate() {
     return false;
+  }
+
+  @Override
+  public void onLoadItemFromResponse(RankResponse rankResponse, List<Rank> items) {
+    super.onLoadItemFromResponse(rankResponse, items);
+    for (Rank rank : items) {
+      if (rank.type == 500) {
+        items.remove(rank);
+        break;
+      }
+    }
   }
 }
